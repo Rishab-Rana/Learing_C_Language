7304a26eadb28da691de111dc4e6e7796a35986d
Lucifer <rishabrana228@gmail.com>
2022-12-15 21:36:07 +0530

    c program to find area of circle

---
+++ area-of-circle/area.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+ 
+int radius;
+printf("enter the radius of the circle\n");
+scanf("%d",&radius);
+int area =  3.14 * radius * radius;
+
+printf("area of the circle is %d\n",area);
+ return 0;
+}
